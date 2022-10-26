099a6b9d52b771ad92330f4bf70ca476e6530a13
natalia-se <flaunder1000@gmail.com>
2022-10-26 18:39:53 +0200

    send message method without auth

--- packages/backend/src/controllers/message-controller.ts
+++ packages/backend/src/controllers/message-controller.ts
@@ -9,17 +9,30 @@ messagesController.get("/", async (req: Request, res: Response<Message[]>) => {
   res.send(await loadMessages());
 });
 
-messagesController.post(
-  "/",
-  async (req: JwtRequest<Message>, res: Response<Message[]>) => {
-    try {
-      const token = req.jwt;
-      if (!token) throw new Error("Missing JWT!");
-      res.send(await sendMessage(req.body, token?.sub));
-    } catch (e) {
-      res.sendStatus(400);
+messagesController.post("/", async (req: Request, res: Response) => {
+  const { text } = req.body;
+  try {
+    if (!text || text == "") {
+      throw new Error("No text in the message!");
     }
+
+    res.send(await sendMessage(req.body));
+  } catch (error) {
+    res.status(400).json({ message: "failed to send message", error: error });
   }
-);
+});
+
+// messagesController.post(
+//   "/",
+//   async (req: JwtRequest<Message>, res: Response<Message[]>) => {
+//     try {
+//       const token = req.jwt;
+//       if (!token) throw new Error("Missing JWT!");
+//       res.send(await sendMessage(req.body, token?.sub));
+//     } catch (e) {
+//       res.sendStatus(400);
+//     }
+//   }
+// );
 
 export default messagesController;
--- packages/backend/src/services/chat-service.ts
+++ packages/backend/src/services/chat-service.ts
@@ -1,17 +1,8 @@
 import { Message } from "@chat-app/shared";
 import { getAll, save } from "../models/message-repository";
 
-export const sendMessage = async (
-  message: Message,
-  author: string
-): Promise<Message[]> => {
-  if (!message.text || message.text == "") {
-    throw new Error("No text in message!");
-  }
-
+export const sendMessage = async (message: Message): Promise<Message[]> => {
   message.timeStamp = new Date();
-  message.author = author;
-
   await save(message);
 
   return await getAll();
