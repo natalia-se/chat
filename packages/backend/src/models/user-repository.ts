3febbcb9b30fe6ed531c5984c98e19cf150eeada
natalia-se <flaunder1000@gmail.com>
2022-10-27 18:54:42 +0200

    use shared interface in user model

--- packages/backend/src/models/user-repository.ts
+++ packages/backend/src/models/user-repository.ts
@@ -1,32 +1,29 @@
 import { model, Schema } from "mongoose";
+import { User } from "@chat-app/shared";
 
-export interface UserInfo {
-  username: string;
-  password: string;
-  name: string;
-  roles: string[];
-}
-
-const UserInfoSchema = new Schema({
+const userSchema = new Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  email: { type: String, required: true },
 });
 
-const UserInfoModel = model<UserInfo>("UserInfo", UserInfoSchema);
+const UserModel = model<User>("User", userSchema);
 
 export const loadUserByUsername = async (
   username: string
-): Promise<UserInfo | null> => {
-  return await UserInfoModel.findOne({ username: username }).exec();
+): Promise<User | null> => {
+  return await UserModel.findOne({ username: username }).exec();
 };
 
-export const saveUserInfo = async (
+export const saveUser = async (
   username: string,
-  hashedPass: any
-): Promise<UserInfo> => {
-  const newUser = new UserInfoModel({
-    username: username,
-    password: hashedPass,
+  email: string,
+  password: any
+): Promise<User> => {
+  const newUser = new UserModel({
+    username,
+    password,
+    email,
   });
 
   return await newUser.save();
