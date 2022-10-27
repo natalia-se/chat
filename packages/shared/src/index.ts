e702aafffc6d64ed73bd8e8d3da3e9e26b73e326
natalia-se <flaunder1000@gmail.com>
2022-10-27 18:52:55 +0200

    add email to user interface and export it

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,2 +1,3 @@
 export type { Credentials } from "./credentials";
 export type { Message } from "./message";
+export type { User } from "./user";
--- packages/shared/src/user.ts
+++ packages/shared/src/user.ts
@@ -1,4 +1,6 @@
 export interface User {
   _id?: string;
   username: string;
+  password: string;
+  email: string;
 }
